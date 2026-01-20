30fb7683f0f3dd39d946b39210c6beccee6769e5
Blake245 <blakewillden23@gmail.com>
2026-01-20 12:48:41 -0700

    Fixed some issues with the random number

--- AgeGuesser.py
+++ AgeGuesser.py
@@ -4,9 +4,9 @@ name_input = input("What is your name?")
 
 while True:
     guess = random.randint(15, 30)
-    guess_input = input("Is your age: " + guess)
+    guess_input = input("Is your age " + str(guess) + " ")
     if guess_input == "y":
-        print(name_input + " is " + guess + "!!!")
+        print(name_input + " is " + str(guess) + "!!!")
         print("Goodbye.")
         break
     elif guess_input == "n":
